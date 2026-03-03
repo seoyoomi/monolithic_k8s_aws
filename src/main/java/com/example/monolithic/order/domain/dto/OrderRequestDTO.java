e995d799cc291a336759aa212753ad9f8b31609c
seoyoomi <seoyoomi1102@gmail.com>
2026-03-03 10:22:57 +0900

    first commit

---
+++ src/main/java/com/example/monolithic/order/domain/dto/OrderRequestDTO.java
@@ -0,0 +1,17 @@
+package com.example.monolithic.order.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequestDTO {
+    private Long productId;
+    private Integer qty;
+}
